1925798710ab1c42069554ca184ede951d220f2b
Ilyas Keskin <ilyas.keskin@posteo.de>
2020-07-10 09:37:43 +0200

    centralizing struct definition

--- agent/agent.go
+++ agent/agent.go
@@ -16,52 +16,6 @@ import (
 	"github.com/mackerelio/go-osstat/network"
 )
 
-// CPUUsage holds CPU usage
-type CPUUsage struct {
-	User   float64 `json:"user"`
-	System float64 `json:"system"`
-	Idle   float64 `json:"idle"`
-}
-
-// MemUsage holds memory usage
-type MemUsage struct {
-	Used   float64 `json:"used"`
-	Cached float64 `json:"cached"`
-	Free   float64 `json:"free"`
-	//SwapUsed  float64 `json:"swap-used"`
-	//SwapFree  float64 `json:"swap-free"`
-}
-
-// NetUsage holds network usage
-type NetUsage struct {
-	Name string  `json:"device"`
-	Rx   float64 `json:"rx"`
-	Tx   float64 `json:"tx"`
-}
-
-// PeerRate holds Wireguard peers date rates
-type PeerRate struct {
-	Rx float64 `json:"rx"`
-	Tx float64 `json:"tx"`
-}
-
-// WGPeer holds wireguard peer information
-type WGPeer struct {
-	IntIPAddr string   `json:"internal-ip"`
-	ExtIPAddr string   `json:"external-ip"`
-	LastHS    int64    `json:"latest-handshake"`
-	PeerRate  PeerRate `json:"data-rates"`
-}
-
-// JSONSkeleton defines the structure of the JSON response
-type JSONSkeleton struct {
-	Hostname  string     `json:"hostname"`
-	CPU       CPUUsage   `json:"cpu"`
-	Memory    MemUsage   `json:"memory"`
-	Network   []NetUsage `json:"network"`
-	Wireguard []WGPeer   `json:"wireguard"`
-}
-
 // getCPUUsage reads the change in the "user"-, "system"- and "idle"-values read from /proc/stats over 1 sec.
 // The values are returned as a percentage of the total CPU usage.
 // If an error occurs while reading those values from the os, an empty object is returned.
--- check/check.go
+++ check/check.go
@@ -13,52 +13,6 @@ import (
 	"github.com/fatih/structs"
 )
 
-// CPUUsage holds CPU usage
-type CPUUsage struct {
-	User   float64 `json:"user"`
-	System float64 `json:"system"`
-	Idle   float64 `json:"idle"`
-}
-
-// MemUsage holds memory usage
-type MemUsage struct {
-	Used   float64 `json:"used"`
-	Cached float64 `json:"cached"`
-	Free   float64 `json:"free"`
-	//SwapUsed  float64 `json:"swap-used"`
-	//SwapFree  float64 `json:"swap-free"`
-}
-
-// NetUsage holds network usage
-type NetUsage struct {
-	Name string  `json:"device"`
-	Rx   float64 `json:"rx"`
-	Tx   float64 `json:"tx"`
-}
-
-// PeerRate holds Wireguard peers date rates
-type PeerRate struct {
-	Rx float64 `json:"rx"`
-	Tx float64 `json:"tx"`
-}
-
-// WGPeer holds wireguard peer information
-type WGPeer struct {
-	IntIPAddr string   `json:"internal-ip"`
-	ExtIPAddr string   `json:"external-ip"`
-	LastHS    int64    `json:"latest-handshake"`
-	PeerRate  PeerRate `json:"data-rates"`
-}
-
-// JSONSkeleton defines the structure of the JSON response
-type JSONSkeleton struct {
-	Hostname  string     `json:"hostname"`
-	CPU       CPUUsage   `json:"cpu"`
-	Memory    MemUsage   `json:"memory"`
-	Network   []NetUsage `json:"network"`
-	Wireguard []WGPeer   `json:"wireguard"`
-}
-
 func getData(host string, port int, path string) JSONSkeleton {
 	resp, err := http.Get("http://" + host + ":" + s.Itoa(port) + path)
 	if err != nil {
