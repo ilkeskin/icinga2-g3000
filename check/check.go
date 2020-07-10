3d115cb5162e31f9995cfc937e674fbb00f42a9a
Ilyas Keskin <ilyas.keskin@posteo.de>
2020-07-10 11:05:28 +0200

    added basic error handling

--- check/check.go
+++ check/check.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	s "strconv"
 	"strings"
@@ -14,88 +14,124 @@ import (
 	"github.com/ilkeskin/icinga-g3000/lib"
 )
 
-func getData(host string, port int, path string) lib.JSONSkeleton {
+func getData(host string, port int, path string) (lib.JSONSkeleton, error) {
+	var skel lib.JSONSkeleton
+
 	resp, err := http.Get("http://" + host + ":" + s.Itoa(port) + path)
 	if err != nil {
-		log.Fatal(err)
+		return skel, err
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 
-	var skel lib.JSONSkeleton
 	json.Unmarshal(body, &skel)
 
-	return skel
+	return skel, nil
 }
 
-func getCPUUsage(data lib.JSONSkeleton) string {
+func getCPUUsage(data lib.JSONSkeleton) (string, error) {
+	var result string
+
+	if !structs.IsStruct(data.CPU) {
+		return result, errors.New("data.CPU is not a struct, empty response?")
+	}
 	m := structs.Map(data.CPU)
-	var result string
+
 	for k, v := range m {
 		result += fmt.Sprintf("'%s'=%s%% ",
 			strings.ToLower(k),
 			s.FormatFloat(v.(float64), 'f', 2, 64))
 	}
-	return strings.TrimSpace(result)
+	return strings.TrimSpace(result), nil
 }
 
-func getMemUsage(data lib.JSONSkeleton) string {
+func getMemUsage(data lib.JSONSkeleton) (string, error) {
+	var result string
+
+	if !structs.IsStruct(data.Memory) {
+		return result, errors.New("data.Memory is not a struct, empty response?")
+	}
 	m := structs.Map(data.Memory)
-	var result string
+
 	for k, v := range m {
 		result += fmt.Sprintf("'%s'=%s%% ",
 			strings.ToLower(k),
 			s.FormatFloat(v.(float64), 'f', 2, 64))
 	}
-	return strings.TrimSpace(result)
+	return strings.TrimSpace(result), nil
 }
 
-func getNICUsage(data lib.JSONSkeleton, nicname string) string {
+func getNICUsage(data lib.JSONSkeleton, nicname string) (string, error) {
+	var result string
+
+	if len(data.Network) == 0 {
+		return result, errors.New("data.Network holds no NICs, empty response?")
+	}
+
 	var m map[string]interface{}
 	for i := range data.Network {
 		if data.Network[i].Name == nicname {
 			m = structs.Map(data.Network[i])
+			result = fmt.Sprintf("'down'=%skbps 'up'=%skbps",
+				s.FormatFloat(m["Rx"].(float64), 'f', 2, 64),
+				s.FormatFloat(m["Tx"].(float64), 'f', 2, 64))
 		}
 	}
 
-	return fmt.Sprintf("'down'=%skbps 'up'=%skbps",
-		s.FormatFloat(m["Rx"].(float64), 'f', 2, 64),
-		s.FormatFloat(m["Tx"].(float64), 'f', 2, 64))
+	return result, nil
 }
 
-func getPeerSecsSinceHS(data lib.JSONSkeleton, peerIndex int64) string {
+func getPeerSecsSinceHS(data lib.JSONSkeleton, peerIndex int64) (string, error) {
+	var result string
+
 	for i := range data.Wireguard {
 		ip := strings.Split(data.Wireguard[i].IntIPAddr, "/")
 		ip = strings.Split(ip[0], ".")
-		idx, _ := s.ParseInt(ip[len(ip)-1], 10, 8)
+
+		idx, err := s.ParseInt(ip[len(ip)-1], 10, 8)
+		if err != nil {
+			return result, err
+		}
+
 		if idx == peerIndex {
-			return fmt.Sprintf("'lasths'=%ds", time.Now().Unix()-data.Wireguard[i].LastHS)
+			result = fmt.Sprintf("'lasths'=%ds", time.Now().Unix()-data.Wireguard[i].LastHS)
+			return result, nil
 		}
 	}
-	return "Error"
+	return result, errors.New("Could not find peer with index " + s.FormatInt(peerIndex, 10) + ",empty response?")
 }
 
-func getPeerUsage(data lib.JSONSkeleton, peerIndex int64) string {
+func getPeerUsage(data lib.JSONSkeleton, peerIndex int64) (string, error) {
+	var result string
+
 	for i := range data.Wireguard {
 		ip := strings.Split(data.Wireguard[i].IntIPAddr, "/")
 		ip = strings.Split(ip[0], ".")
-		idx, _ := s.ParseInt(ip[len(ip)-1], 10, 8)
+
+		idx, err := s.ParseInt(ip[len(ip)-1], 10, 8)
+		if err != nil {
+			return result, err
+		}
+
 		if idx == peerIndex {
-			return fmt.Sprintf("'down'=%.2fkbps 'up'=%.2fkbps",
-				data.Wireguard[i].PeerRate.Rx,
-				data.Wireguard[i].PeerRate.Tx)
+			result = fmt.Sprintf("'down'=%.2fkbps 'up'=%.2fkbps", data.Wireguard[i].PeerRate.Rx, data.Wireguard[i].PeerRate.Tx)
+			return result, nil
 		}
 	}
-	return "Error"
+	return result, errors.New("Could not find peer with index " + s.FormatInt(peerIndex, 10) + ",empty response?")
 }
 
 func main() {
-	data := getData("127.0.0.1", 8888, "/test.json")
+	data, err := getData("127.0.0.1", 8888, "/test.json")
 
 	fmt.Println(getCPUUsage(data))
 	fmt.Println(getMemUsage(data))
 	fmt.Println(getNICUsage(data, "eth1"))
 	fmt.Println(getPeerSecsSinceHS(data, 9))
 	fmt.Println(getPeerUsage(data, 12))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
